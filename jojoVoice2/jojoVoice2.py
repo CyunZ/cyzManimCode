93aae72bf97db653f7539cd123bda536d7676dfa
cyz <526438040@qq.com>
2020-02-25 20:17:52 +0800

    添加若干场景

--- jojoVoice2/jojoVoice2.py
+++ jojoVoice2/jojoVoice2.py
@@ -1,17 +1,18 @@
 from manimlib.imports import *
 import mytool.FFT_Song as FFT_Song
 
+# 简单举例波形叠加
 class Part0(Scene):
     def construct(self):
         fun1 = lambda x: np.cos(2 * PI *x * 2)
         fg1 = FunctionGraph(fun1,color=MAROON_B)
-        equation1 = TexMobject("y=cos(2\\pi x\\times 2)")
+        equation1 = TexMobject("y_1=cos(2\\pi x\\times 2)")
         equation1.set_y(2 - FRAME_Y_RADIUS - 0.5)
         g1 = VGroup(fg1,equation1)
-
+        
         fun2 = lambda x: np.cos(2 * PI *x * 4 + 45/180*PI)
-        fg2 = FunctionGraph(fun2,color=RED_E)
-        equation2 = TexMobject("y=cos(2\\pi x\\times 4+{45\\over 180}\\pi )")
+        fg2 = FunctionGraph(fun2,color=BLUE_E)
+        equation2 = TexMobject("y_2=cos(2\\pi x\\times 4+{45\\over 180}\\pi )")
         equation2.set_y(2 - FRAME_Y_RADIUS -0.5)
         g2 = VGroup(fg2,equation2)
 
@@ -19,12 +20,13 @@ class Part0(Scene):
                 np.cos(2 * PI *x * 2),
                 np.cos(2 * PI *x * 4 + 45/180*PI)
             ])
-        fg3 = FunctionGraph(fun3,color="#00FF33")
-        equation3 = TexMobject("y=cos(2\\pi x\\times 2)+cos(2\\pi x\\times 4+{45\\over 180}\\pi )")
+        fg3 = FunctionGraph(fun3,color=GREEN_C)
+        equation3 = TexMobject("y_3=cos(2\\pi x\\times 2)+cos(2\\pi x\\times 4+{45\\over 180}\\pi )")
         equation3.set_y(2 - FRAME_Y_RADIUS - 0.5)
         g3 = VGroup(fg3,equation3)
 
-        axes1 = Axes(y_min=-1, y_max=1.5)
+        axes1 = Axes(y_min=-1.5, y_max=1.5)
+        copy_axes1 = axes1.copy()
         axes2 = Axes(y_min=-2, y_max=2.5)
         self.play( Write( axes1 ) )
         self.wait(1)
@@ -48,53 +50,163 @@ class Part0(Scene):
         self.play(ShowCreation(g3))
         self.wait(2)
 
-class Part0_5(MovingCameraScene):
-    def construct(self):
-        fun1 = lambda x: np.cos(2 * PI *x * 60)
-        fg1 = FunctionGraph(fun1,color=MAROON_B)
-
-        fun2 = lambda x: np.cos(2 * PI *x * 0.6)
-        fg2 = FunctionGraph(fun2,color=RED_E)
-        
-        self.play(ShowCreation(fg1))
-        self.play(Transform(fg1,fg2))
-        self.wait(2)
-
-class Part1(MovingCameraScene):
-    def construct(self):
-
-        frame_width = 50
-        
-        params = FFT_Song.getSongFormula("jojoVoice2.wav",200)
-        print(params)
-        self.camera.set_frame_center((FRAME_X_RADIUS,0,0))
-        self.camera.set_frame_height(1000)
-        self.camera.set_frame_width(frame_width)
-        fun = lambda x: sum([
-                param[0] * np.sin(2 * PI *param[1]* x + param[2]/180*PI)
-                for param in params
-            ])
-        # for i in range(1):
-        #     f = FunctionGraph(
-        #         fun,
-        #         color=GREEN,
-        #         x_max=100 * (i+1),
-        #         x_min=100 * i
-        #     )
-        #     self.play(ShowCreation(f))
-
-        curve = VMobject()
-        curve.set_points_smoothly([
-            (a,0.1*fun(a),0)
-            for a in np.linspace(0, frame_width, 10000)
-        ])
-        curve.set_stroke(YELLOW, 1)
-        # self.play(ShowCreation(curve))
-        self.add(curve)
-        self.play(ShowCreation(Axes()))
+        self.play(ApplyMethod( g3.scale,0.4) )
         self.play(
-            ApplyMethod(self.camera.frame.set_height,5),
-            ApplyMethod(self.camera.frame.set_width,10),
+            ApplyMethod( g1.shift,( FRAME_X_RADIUS-3, -1*FRAME_Y_RADIUS ,0) ),
+            ApplyMethod( g3.shift,( 3-FRAME_X_RADIUS, FRAME_Y_RADIUS-0.8 ,0) ),
+        )
+        minus = TexMobject("-")
+        minus.set_y(FRAME_Y_RADIUS-0.5)
+        self.play(
+            ApplyMethod( g1.scale,10/4),
+            Transform(add,minus),
+            Transform(axes1,copy_axes1)
         )
         self.wait(2)
+
+#延伸到声波
+class Part0_5(MovingCameraScene):
+    def construct(self):
+        fun1 = lambda x: np.cos(2 * PI *x * 220)
+        fg1 = FunctionGraph(fun1,color=MAROON_B)
+        equation1 = TexMobject("y_1=cos(2\\pi x\\times 220)")
+        equation1.set_y(2 - FRAME_Y_RADIUS - 0.5)
+        g1 = VGroup(fg1,equation1)
+
+        fun2 = lambda x: np.cos(2 * PI *x * 0.22)
+        fg2 = FunctionGraph(fun2,color=MAROON_B)
         
+        self.play(ShowCreation(g1))
+        self.wait(2)
+        self.play(ApplyMethod(self.camera.frame.set_width,5))
+        self.wait(2)
+        self.play(ApplyMethod(self.camera.frame.set_width,FRAME_WIDTH))
+        self.wait(2)
+
+        self.play(Transform(fg1,fg2))
+        self.wait(2)
+        self.play(ApplyMethod( g1.scale,0.4) )
+        self.play( ApplyMethod( g1.shift,( 3-FRAME_X_RADIUS, FRAME_Y_RADIUS ,0) ) )
+        self.wait(2)
+
+        fun3 = lambda x: np.cos(2 * PI *x * 1080)
+        fg3 = FunctionGraph(fun3,color=BLUE_E)
+        equation3 = TexMobject("y_2=cos(2\\pi x\\times 1080)")
+        equation3.set_y(2 - FRAME_Y_RADIUS -0.5)
+        g3 = VGroup(fg3,equation3)
+
+        fun4 = lambda x: np.cos(2 * PI *x * 1.08)
+        fg4 = FunctionGraph(fun4,color=BLUE_E)
+        
+        self.play(ShowCreation(g3))
+        self.wait(2)
+        self.play(Transform(fg3,fg4))
+        self.wait(2)
+        self.play(ApplyMethod( g3.scale,0.4) )
+        self.play( ApplyMethod( g3.shift,( FRAME_X_RADIUS - 3, FRAME_Y_RADIUS ,0) ) )
+        self.wait(2)
+
+        add = TexMobject("+")
+        add.set_y(FRAME_Y_RADIUS-0.5)
+        self.play(ShowCreation(add))
+        self.wait(1)
+
+        fun5 = lambda x: sum([
+                np.cos(2 * PI *x * 2),
+                np.cos(2 * PI *x * 4 + 45/180*PI)
+            ])
+        fg5 = FunctionGraph(fun5,color=GREEN_C)
+        equation5 = TexMobject("y_3=cos(2\\pi x\\times 220)+cos(2\\pi x\\times 1080)")
+        equation5.set_y(2 - FRAME_Y_RADIUS - 0.5)
+        g5 = VGroup(fg5,equation5)
+        self.play(ShowCreation(g5))
+        self.wait(2)
+        
+        self.play(ApplyMethod( g5.scale,0.4) )
+        self.play(
+            ApplyMethod( g1.shift,( FRAME_X_RADIUS-3, -1*FRAME_Y_RADIUS ,0) ),
+            ApplyMethod( g5.shift,( 3-FRAME_X_RADIUS, FRAME_Y_RADIUS-0.8 ,0) ),
+        )
+        minus = TexMobject("-")
+        minus.set_y(FRAME_Y_RADIUS-0.5)
+        self.play(
+            ApplyMethod( g1.scale,10/4),
+            Transform(add,minus),
+        )
+        self.wait(2)
+
+
+# 实际应用 引出FFT
+class Part1(MovingCameraScene):
+    def construct(self):
+        data,Fs,N = FFT_Song.getSongData("jojoVoice2.wav")
+
+        x_scale = 0.01
+        y_max = 1000
+        frame_width = len(data) * x_scale
+        self.camera.set_frame_center((frame_width/2,0,0))
+        self.camera.set_frame_height(y_max * 2)
+        self.camera.set_frame_width(frame_width)
+        
+        curve = VMobject()
+        max_amplitude = max(data)
+        curve.set_points_smoothly([
+            (x_scale*i,data[i] / max_amplitude * y_max,0) 
+            for i in range(len(data))
+        ])
+
+        curve.set_stroke(GREEN_C, 0.5)
+        self.play(ShowCreation(curve))
+        # self.add(curve)
+        # self.play(ShowCreation(Axes()))
+        self.wait(2)
+
+class Part1_2(MovingCameraScene):
+    def construct(self):
+        params = FFT_Song.getSongFormula("jojoVoice2.wav",1)
+        # fun = lambda x: sum([
+        #         param[0] * np.sin(2 * PI *param[1]* x + param[2]/180*PI)
+        #         for param in params
+        #     ])
+
+        texes = []
+        str1 = "y="
+        for i in range(3):
+            param = params[i]
+            str1 += "{}cos(2\\pi x\\times {} ".format(param[0] if (param[0] != 1) else '',param[1])
+            if param[2] > 0:
+                str1 += "+"
+            else :
+                str1 += "-"
+            str1 += "{"+ str( abs(param[2]) ) + "\\over 180}\\pi)"
+        tex1 = TexMobject(str1)
+        tex1.scale(0.6)
+        tex1.set_y(3.5)
+        texes.append(tex1)
+
+        for k in range(6):
+            str2 = "+"
+            for i in range((k+1)*3,(k+2)*3):
+                param = params[i]
+                str2 += "{}cos(2\\pi x\\times {} ".format(param[0],param[1])
+                if param[2] > 0:
+                    str2 += "+"
+                else :
+                    str2 += "-"
+                str2 += "{"+ str( abs(param[2]) ) + "\\over 180}\\pi)"
+                if i%3 < 2:
+                    str2 += "+"
+            tex2 = TexMobject(str2)
+            tex2.scale(0.6)
+            tex2.set_y(2.5-k)
+            texes.append(tex2)
+
+        str3 = "+\\cdots \\cdots \\cdots"
+        tex3 = TexMobject(str3)
+        tex3.scale(0.6)
+        tex3.set_y(2.5-6)
+        texes.append(tex3)
+
+        g = VGroup(*texes)
+        self.play(ShowCreation(g))
+        self.wait(2)
--- mytool/FFT_Song.py
+++ mytool/FFT_Song.py
@@ -6,6 +6,23 @@ from scipy import signal
 import math
 
 
+def getSongData(wavfile_name):
+    """
+    取得声波数据
+    """
+    song = AudioSegment.from_wav(wavfile_name)
+    nchannels = song.channels #声道数
+    sample_width = song.sample_width #位深
+    Fs = song.frame_rate #采样率
+    N = int(song.frame_count()) #采样点数量
+
+    y = [0]*N
+    for i in range(N):
+        val = song.get_frame(i)
+        left = val[0:sample_width] #取左声道数据 默认双声道 位深16bit
+        v = struct.unpack('h',left)[0]
+        y[i] = v
+    return y,Fs,N
 
 def getSongFormula(wavfile_name,filter_amplitude=50):
     """
@@ -15,20 +32,7 @@ def getSongFormula(wavfile_name,filter_amplitude=50):
         返回值 [(强度,频率,相位(角度))]
         例如 [(75, 44, -132), (56, 47, 110)]
     """
-    song = AudioSegment.from_wav(wavfile_name)
-    nchannels = song.channels #声道数
-    sample_width = song.sample_width #位深
-    Fs = song.frame_rate #采样率
-    N = int(song.frame_count()) #采样点数量
-    
-    #取得声波数据
-    y = [0]*N
-    for i in range(N):
-        val = song.get_frame(i)
-        left = val[0:2] #取左声道数据 默认双声道 位深16bit
-        v = struct.unpack('h',left)[0]
-        y[i] = v
-
+    y,Fs,N = getSongData(wavfile_name)
     yy = fft(y)
     abs_yf = np.abs(yy)
 
@@ -38,12 +42,12 @@ def getSongFormula(wavfile_name,filter_amplitude=50):
 
     for i in range(max_fre):
         index_i_hz = i * N // Fs # 波形的频率 = index * Fs / N，倒推计算索引：index = 波形频率 * N / Fs
-        frequencys[i] = int( round( abs_yf[index_i_hz] * 2.0/N ) )  # 弦波分量的振幅放大了N/2倍
-        if frequencys[i] > filter_amplitude: # 过滤赋值低于50的频率信号
+        frequencys[i] = round( abs_yf[index_i_hz] * 2.0/N , 2)   # 弦波分量的振幅放大了N/2倍
+        if frequencys[i] > filter_amplitude: # 过滤幅值低于指定值的频率信号
             if i == 0: # 直流分量
                 frequencys[i] /= 2
             else : # 交流分量
-                phase = int( round( np.angle( yy[index_i_hz],True ) ) ) # 计算相位
+                phase =  round( np.angle( yy[index_i_hz],True ) , 2)  # 计算相位
                 params.append((frequencys[i],i,phase))
                 # print("{}cos(2pi x {} + {}/180pi)".format(frequencys[i],i,phase))
     
