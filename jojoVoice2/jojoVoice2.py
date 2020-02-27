78443454214577c5a8e8c3002d465ee4390f54a8
cyz <526438040@qq.com>
2020-02-27 17:14:55 +0800

    修改频率获取算法 添加若干场景

--- jojoVoice2/jojoVoice2.py
+++ jojoVoice2/jojoVoice2.py
@@ -112,8 +112,8 @@ class Part0_5(MovingCameraScene):
         self.wait(1)
 
         fun5 = lambda x: sum([
-                np.cos(2 * PI *x * 2),
-                np.cos(2 * PI *x * 4 + 45/180*PI)
+                np.cos(2 * PI *x * 0.22),
+                np.cos(2 * PI *x * 1.08)
             ])
         fg5 = FunctionGraph(fun5,color=GREEN_C)
         equation5 = TexMobject("y_3=cos(2\\pi x\\times 220)+cos(2\\pi x\\times 1080)")
@@ -135,8 +135,8 @@ class Part0_5(MovingCameraScene):
         )
         self.wait(2)
 
-
-# 实际应用 引出FFT
+#Part1 实际应用 引出FFT
+# 声波图  
 class Part1(MovingCameraScene):
     def construct(self):
         data,Fs,N = FFT_Song.getSongData("jojoVoice2.wav")
@@ -161,6 +161,7 @@ class Part1(MovingCameraScene):
         # self.play(ShowCreation(Axes()))
         self.wait(2)
 
+# 列出方程
 class Part1_2(MovingCameraScene):
     def construct(self):
         params = FFT_Song.getSongFormula("jojoVoice2.wav",1)
@@ -210,3 +211,415 @@ class Part1_2(MovingCameraScene):
         g = VGroup(*texes)
         self.play(ShowCreation(g))
         self.wait(2)
+
+# 画出BGM声波图
+class Part1_3(Scene):
+    def construct(self):
+        data,Fs,N = FFT_Song.getSongData("jojoVoice2bgm.wav")
+
+        x_scale = 0.01
+        y_max = 1000
+        frame_width = len(data) * x_scale
+        self.camera.set_frame_center((frame_width/2,0,0))
+        self.camera.set_frame_height(y_max * 2)
+        self.camera.set_frame_width(frame_width)
+        
+        curve = VMobject()
+        max_amplitude = max(data)
+        curve.set_points_smoothly([
+            (x_scale*i,data[i] / max_amplitude * y_max,0) 
+            for i in range(len(data))
+        ])
+
+        curve.set_stroke(MAROON_B, 0.5)
+        self.play(ShowCreation(curve))
+        # self.add(curve)
+        # self.play(ShowCreation(Axes()))
+        self.wait(2)
+
+# 列出BGM方程
+class Part1_4(Scene):
+    def construct(self):
+        params = FFT_Song.getSongFormula("jojoVoice2bgm.wav",1)
+
+        texes = []
+        str1 = "y="
+        for i in range(3):
+            param = params[i]
+            str1 += "{}cos(2\\pi x\\times {} ".format(param[0] if (param[0] != 1) else '',param[1])
+            if param[2] > 0:
+                str1 += "+"
+            else :
+                str1 += "-"
+            str1 += "{"+ str( abs(param[2]) ) + "\\over 180}\\pi)"
+        tex1 = TexMobject(str1)
+        tex1.scale(0.6)
+        tex1.set_y(3.5)
+        texes.append(tex1)
+
+        for k in range(6):
+            str2 = "+"
+            for i in range((k+1)*3,(k+2)*3):
+                param = params[i]
+                str2 += "{}cos(2\\pi x\\times {} ".format(param[0],param[1])
+                if param[2] > 0:
+                    str2 += "+"
+                else :
+                    str2 += "-"
+                str2 += "{"+ str( abs(param[2]) ) + "\\over 180}\\pi)"
+                if i%3 < 2:
+                    str2 += "+"
+            tex2 = TexMobject(str2)
+            tex2.scale(0.6)
+            tex2.set_y(2.5-k)
+            texes.append(tex2)
+
+        str3 = "+\\cdots \\cdots \\cdots"
+        tex3 = TexMobject(str3)
+        tex3.scale(0.6)
+        tex3.set_y(2.5-6)
+        texes.append(tex3)
+
+        g = VGroup(*texes)
+        self.play(ShowCreation(g))
+        self.wait(2)
+
+# 删除BGM后的声波
+class Part1_5(Scene):
+    def construct(self):
+        data,Fs,N = FFT_Song.getSongData("joDelBGM.wav")
+
+        x_scale = 0.01
+        y_max = 1000
+        frame_width = len(data) * x_scale
+        self.camera.set_frame_center((frame_width/2,0,0))
+        self.camera.set_frame_height(y_max * 2)
+        self.camera.set_frame_width(frame_width)
+        
+        curve = VMobject()
+        max_amplitude = max(data)
+        curve.set_points_smoothly([
+            (x_scale*i,data[i] / max_amplitude * y_max,0) 
+            for i in range(len(data))
+        ])
+
+        curve.set_stroke(BLUE_E, 0.5)
+        self.play(ShowCreation(curve))
+        # self.add(curve)
+        # self.play(ShowCreation(Axes()))
+        self.wait(2)
+
+# 频谱
+class Part1_6(Scene):
+    def construct(self):
+        params = FFT_Song.getSongFormula2("jojoVoice2.wav",0)
+        np_params = np.array(params)
+        freq = np.array([0]*5000) # 取前5000个频率及其对应强度
+        amplitudes =np.array([0]*5000) 
+        for i in range(5000):
+            p = params[i]
+            freq[i] = p[1]
+            amplitudes[i] = p[0]
+
+        y_max = FRAME_Y_RADIUS
+        x_max = FRAME_WIDTH
+        self.camera.set_frame_center((x_max/2,0,0))
+        self.camera.set_frame_height(y_max * 2 + 6)
+        self.camera.set_frame_width(x_max + 1)
+
+        #  max_freq = 5000
+        # max_amplitude = 500
+
+        axes = Axes(
+           x_min = 0, x_max= x_max+0.5,
+           y_min = 0, y_max= y_max+0.5,
+        )
+        y_tex = TextMobject("强度")
+        y_tex.set_x(0.5)
+        y_tex.set_y(y_max)
+        y_tex.scale(0.7)
+        x_tex = TextMobject("频率")
+        x_tex.set_x(x_max)
+        x_tex.set_y(0.5)
+        x_tex.scale(0.7)
+        title_tex = TextMobject("原音频频谱")
+        title_tex.set_stroke(GREEN_C,1)
+        title_tex.scale(0.7)
+        title_tex.set_x(x_max/2)
+        title_tex.set_y(y_max)
+       
+        g = VGroup(axes,y_tex,x_tex,title_tex)
+        g.shift( (0,1,0) )
+        curve = VMobject()
+        max_freq = max(freq)
+        max_amplitude = max(amplitudes)
+        curve.set_points_smoothly([
+            (freq[i] / max_freq * x_max,amplitudes[i] / max_amplitude * y_max,0) 
+            for i in range(len(freq))
+        ])
+        curve.set_stroke(GREEN_C, 0.6)
+        curve.shift( (0,1,0) )
+        
+        # self.add(g)
+        self.play(ShowCreation(g))
+        self.play(ShowCreation(curve))
+        params = FFT_Song.getSongFormula2("joDelBGM.wav",0)
+        freq = [0] * 5000 # 取0~5000Hz的强度
+        amplitudes = [0] * 5000
+        for i in range(5000):
+            p = params[i]
+            freq[i] = p[1]
+            amplitudes[i] = p[0]
+
+        axes = Axes(
+           x_min = 0, x_max= x_max+0.5,
+           y_min = 0, y_max= y_max+0.5,
+           center_point=(0,-y_max - 2,0)
+        )
+        y_tex = TextMobject("强度")
+        y_tex.set_x(0.5)
+        y_tex.set_y(- 2)
+        y_tex.scale(0.7)
+        x_tex = TextMobject("频率")
+        x_tex.set_x(x_max)
+        x_tex.set_y(0.5-y_max - 2) 
+        x_tex.scale(0.7)
+        title_tex = TextMobject("处理后音频频谱")
+        title_tex.set_stroke(BLUE_E,0.7)
+        title_tex.scale(0.7)
+        title_tex.set_x(x_max/2)
+        title_tex.set_y(y_max-y_max - 2)
+        g = VGroup(axes,y_tex,x_tex,title_tex)
+        
+        g.shift( (0,1,0) )
+        curve = VMobject()
+        max_amplitude = max(amplitudes)
+        curve.set_points_smoothly([
+            (freq[i] / max_freq * x_max,amplitudes[i] / max_amplitude * y_max,0) 
+            for i in range(len(freq))
+        ])
+        curve.set_stroke(BLUE_E, 0.6)
+        curve.set_y(-0.5*y_max - 2)
+        curve.shift( (0,1,0) )
+        # self.add(curve)
+        self.play(ShowCreation(g))
+        self.play(ShowCreation(curve))
+        self.wait(2)
+
+# BGM频谱图
+class Part1_7(Scene):
+    def construct(self):
+        params = FFT_Song.getSongFormula2("jo2BGM.wav",0)
+        np_params = np.array(params)
+        freq = np.array([0]*5000) # 取前5000个频率及其对应强度
+        amplitudes =np.array([0]*5000) 
+        for i in range(5000):
+            p = params[i]
+            freq[i] = p[1]
+            amplitudes[i] = p[0]
+
+        y_max = FRAME_Y_RADIUS
+        x_max = FRAME_WIDTH
+        self.camera.set_frame_center((x_max/2,y_max/2+1,0))
+        self.camera.set_frame_height(y_max + 2)
+        self.camera.set_frame_width(x_max + 1)
+
+        #  max_freq = 5000
+        # max_amplitude = 500
+
+        axes = Axes(
+           x_min = 0, x_max= x_max+0.5,
+           y_min = 0, y_max= y_max+0.5,
+        )
+        y_tex = TextMobject("强度")
+        y_tex.set_x(0.5)
+        y_tex.set_y(y_max)
+        y_tex.scale(0.7)
+        x_tex = TextMobject("频率")
+        x_tex.set_x(x_max)
+        x_tex.set_y(0.5)
+        x_tex.scale(0.7)
+        title_tex = TextMobject("BGM频谱")
+        title_tex.set_stroke(MAROON_B,1)
+        title_tex.scale(0.7)
+        title_tex.set_x(x_max/2)
+        title_tex.set_y(y_max)
+       
+        g = VGroup(axes,y_tex,x_tex,title_tex)
+        g.shift( (0,1,0) )
+        curve = VMobject()
+        max_freq = max(freq)
+        max_amplitude = max(amplitudes)
+        curve.set_points_smoothly([
+            (freq[i] / max_freq * x_max,amplitudes[i] / max_amplitude * y_max,0) 
+            for i in range(len(freq))
+        ])
+        curve.set_stroke(MAROON_B, 0.6)
+        curve.shift( (0,1,0) )
+        
+        # self.add(g)
+        self.play(ShowCreation(g))
+        self.play(ShowCreation(curve))
+        self.wait(2)
+
+# 不同波相减
+class Part1_8(Scene):
+    def construct(self):
+        fun1 = lambda x: np.cos(2 * PI *x * 0.22)
+        fg1 = FunctionGraph(fun1,color=MAROON_B)
+        equation1 = TexMobject("y_1=cos(2\\pi x\\times 220)")
+        equation1.set_y(2 - FRAME_Y_RADIUS - 0.5)
+        g1 = VGroup(fg1,equation1)
+        
+        fun2 = lambda x: np.cos(2 * PI *x * 1.08)
+        fg2 = FunctionGraph(fun2,color=BLUE_E)
+        equation2 = TexMobject("y_2=cos(2\\pi x\\times 1080)")
+        equation2.set_y(2 - FRAME_Y_RADIUS -0.5)
+        g2 = VGroup(fg2,equation2)
+
+        fun3 = lambda x: np.cos(2 * PI *x * 0.22) -  np.cos(2 * PI *x * 1.08)
+        fg3 = FunctionGraph(fun3,color=GREEN_C)
+        equation3 = TexMobject("y_3=cos(2\\pi x\\times 220)-cos(2\\pi x\\times 1080)")
+        equation3.set_y(2 - FRAME_Y_RADIUS - 0.5)
+        g3 = VGroup(fg3,equation3)
+
+        self.play(ShowCreation(g1))
+        self.wait(2)
+        self.play(ApplyMethod( g1.scale,0.4) )
+        self.play( ApplyMethod( g1.shift,( 3-FRAME_X_RADIUS, FRAME_Y_RADIUS ,0) ) )
+        self.wait(2)
+        self.play(ShowCreation(g2))
+        self.wait(2)
+        self.play(ApplyMethod( g2.scale,0.4) )
+        self.play( ApplyMethod( g2.shift,( FRAME_X_RADIUS - 3, FRAME_Y_RADIUS ,0) ) )
+        self.wait(2)
+
+        add = TexMobject("-")
+        add.set_y(FRAME_Y_RADIUS-0.5)
+        self.play(ShowCreation(add))
+        self.wait(1)
+
+        self.play(ShowCreation(g3))
+        self.wait(2)
+       
+# 带通滤波
+class Part2(Scene):
+    def construct(self):
+        params = FFT_Song.getSongFormula2("jojoVoice2.wav",0)
+        
+        np_params = np.array(params)
+        freq = np.array([0]*4200) # 取前5000个频率及其对应强度
+        amplitudes =np.array([0]*4200) 
+        for i in range(4200):
+            p = params[i]
+            freq[i] = p[1]
+            amplitudes[i] = p[0]
+
+        y_max = FRAME_Y_RADIUS
+        x_max = FRAME_WIDTH
+        self.camera.set_frame_center((x_max/2,y_max/2+1,0))
+        self.camera.set_frame_height(y_max + 2)
+        self.camera.set_frame_width(x_max + 1)
+
+        axes = Axes(
+           x_min = 0, x_max= x_max+0.5,
+           y_min = 0, y_max= y_max+0.5,
+        )
+        y_tex = TextMobject("强度")
+        y_tex.set_x(0.5)
+        y_tex.set_y(y_max)
+        y_tex.scale(0.7)
+        x_tex = TextMobject("频率")
+        x_tex.set_x(x_max)
+        x_tex.set_y(0.5)
+        x_tex.scale(0.7)
+        title_tex = TextMobject("原音频频谱")
+        title_tex.set_stroke(MAROON_B,1)
+        title_tex.scale(0.7)
+        title_tex.set_x(x_max/2)
+        title_tex.set_y(y_max)
+       
+        g = VGroup(axes,y_tex,x_tex,title_tex)
+        g.shift( (0,1,0) )
+        max_freq = max(freq)
+        max_amplitude = max(amplitudes)
+        curve = VMobject()
+        curve.set_points_smoothly([
+            (freq[i] / max_freq * x_max,amplitudes[i] / max_amplitude * y_max,0) 
+            for i in range(len(freq))
+        ])
+
+        print(max_freq)
+        numTexs = []
+        for i in range(14):
+            num = 300*(i+1)
+            numTex = TextMobject(str(num))
+            numTex.shift( ( i+1 , 0.7 , 0) )
+            numTex.scale(0.4)
+            numTexs.append(numTex)
+        numGroup = VGroup(*numTexs)
+
+        curve.set_stroke(MAROON_B, 0.6)
+        curve.shift( (0,1,0) )
+        self.play(ShowCreation(g))
+        self.play(ShowCreation(numGroup))
+        self.play(ShowCreation(curve))
+        self.wait(2)
+        
+        params = FFT_Song.getSongFormula2("jo65_1100.wav",0)
+        freq = np.array([0]*5000) # 取前5000个频率及其对应强度
+        amplitudes =np.array([0]*5000) 
+        for i in range(5000):
+            p = params[i]
+            freq[i] = p[1]
+            amplitudes[i] = p[0]
+        curve2 = VMobject()
+        curve2.set_points_smoothly([
+            (freq[i] / max_freq * x_max,amplitudes[i] / max_amplitude * y_max,0) 
+            for i in range(len(freq))
+        ])
+        curve2.shift((0,1,0))
+        curve2.set_stroke(GREEN_C, 0.6)
+        title_tex2 = TextMobject("保留原音频65-1100Hz后的频谱")
+        title_tex2.set_stroke(GREEN_C,1)
+        title_tex2.scale(0.7)
+        title_tex2.set_x(x_max/2)
+        title_tex2.set_y(y_max+1)
+
+        self.play(
+            Transform(curve,curve2),
+            Transform(title_tex,title_tex2)
+        )
+        self.wait(2)
+
+        params = FFT_Song.getSongFormula2("jo270_3000.wav",0)
+        freq = np.array([0]*5000) # 取前5000个频率及其对应强度
+        amplitudes =np.array([0]*5000) 
+        for i in range(5000):
+            p = params[i]
+            freq[i] = p[1]
+            amplitudes[i] = p[0]
+        curve2 = VMobject()
+        curve2.set_points_smoothly([
+            (freq[i] / max_freq * x_max,amplitudes[i] / max_amplitude * y_max,0) 
+            for i in range(len(freq))
+        ])
+        curve2.shift((0,1,0))
+        curve2.set_stroke(BLUE_E, 0.6)
+
+        title_tex2 = TextMobject("保留原音频270-3000Hz后的频谱")
+        title_tex2.set_stroke(BLUE_E,1)
+        title_tex2.scale(0.7)
+        title_tex2.set_x(x_max/2)
+        title_tex2.set_y(y_max+1)
+
+        self.play(
+            Transform(curve,curve2),
+            Transform(title_tex,title_tex2)
+        )
+        self.wait(2)
+
+
+class Part3(Scene):
+    def construct(self):
+        pass
--- mytool/FFT_Song.py
+++ mytool/FFT_Song.py
@@ -53,6 +53,32 @@ def getSongFormula(wavfile_name,filter_amplitude=50):
     
     return params
 
+def getSongFormula2(wavfile_name,filter_amplitude=50):
+    """
+    返回音频的余弦函数表达式参数
+        wavfile_name:文件名
+        filter_amplitude:过滤掉该强度以下的频率
+        返回值 [(强度,频率,相位(角度))]
+        例如 [(75, 44, -132), (56, 47, 110)]
+    """
+    y,Fs,N = getSongData(wavfile_name)
+    yy = fft(y)
+    abs_yf = np.abs(yy)
+
+    max_fre = Fs / 2
+    params = []
+    dc = abs_yyl[0] /N # 直流分量 被放大了N倍
+
+    for i in range(1,len(abs_yf)):
+        freq = i*Fs/N   # 频率 = index * Fs / N
+        if freq > max_fre: # 最大频率不会超过采样频率的一般
+            break
+        amplitude = abs_yyl[i] * 2.0/N
+        if amplitude > filter_amplitude: # 过滤幅值低于指定值的频率信号
+                phase =  np.angle( yy[index_i_hz],True ) # 计算相位
+                params.append((amplitude,freq,phase))
+    return params
+
 def getWavDataFormula(data,Fs,filter_amplitude=50):
     """
     返回音频的余弦函数表达式参数
@@ -74,19 +100,17 @@ def getWavDataFormula(data,Fs,filter_amplitude=50):
     yy = fft(y)
     abs_yf = np.abs(yy)
 
-    max_fre = Fs // 2
-    frequencys = [0] * (max_fre)
+    max_fre = Fs / 2
     params = []
+    dc = abs_yyl[0] /N # 直流分量 被放大了N倍
 
-    for i in range(max_fre):
-        index_i_hz = i * N // Fs # 波形的频率 = index * Fs / N，倒推计算索引：index = 波形频率 * N / Fs
-        frequencys[i] = int( round( abs_yf[index_i_hz] * 2.0/N ) )  # 弦波分量的振幅放大了N/2倍
-        if frequencys[i] > filter_amplitude: # 过滤赋值低于50的频率信号
-            if i == 0: # 直流分量
-                frequencys[i] /= 2
-            else : # 交流分量
-                phase = int( round( np.angle( yy[index_i_hz],True ) ) ) # 计算相位
-                params.append((frequencys[i],i,phase))
-                # print("{}cos(2pi x {} + {}/180pi)".format(frequencys[i],i,phase))
+    for i in range(1,len(abs_yf)):
+        freq = i*Fs/N   # 频率 = index * Fs / N
+        if freq > max_fre: # 最大频率不会超过采样频率的一般
+            break
+        amplitude = abs_yyl[i] * 2.0/N
+        if amplitude > filter_amplitude: # 过滤幅值低于指定值的频率信号
+                phase =  np.angle( yy[index_i_hz],True ) # 计算相位
+                params.append((amplitude,freq,phase))
     
-    return params,frequencys
+    return params
